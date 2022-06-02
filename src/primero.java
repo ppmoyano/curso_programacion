fe83e6dafe48e914ec5f29f4b6309b570f734fba
Pepe Moyano <josemariamoyano.ar@gmail.com>
2022-06-02 21:00:50 +0200

    agregado primera informacion

--- src/primero.java
+++ src/primero.java
@@ -3,4 +3,27 @@ public class primero {
     public static void main(String[] args) {
 
     }
+    /*
+     TIPO DE DATOS:
+     * Nativos:
+     - Numerico
+        byte
+        short
+        int
+        float
+        double
+        long
+     - Caracter
+        char
+     - Booleano (logico)
+        boolean
+     * Objetos - instancia de una clase
+       String -> Cadena de caracteres
+
+     *Loops / Condicionales
+        if ()
+          ...
+          else
+          ....
+     */
 }
