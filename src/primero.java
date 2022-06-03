133ffd750f75599a5be066ee21d10cbcd667e59f
Pepe <josemariamoyano.ar@gmail.com>
2022-06-03 18:25:51 +0200

    Agregado el primer ejemplo

--- src/primero.java
+++ src/primero.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class primero {
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int notaA;
+        int notaB;
+        int promedio;
+
+        System.out.println("Ingrese la primer nota: ");
+        notaA = teclado.nextInt();
+        System.out.println("Ingrese la segunda nota: ");
+        notaB = teclado.nextByte();
+
+        promedio = (notaA + notaB) / 2;
+
+        if ( promedio >= 6) {
+            System.out.println("APROBADO");
+        } else {
+            System.out.println("DESAPROBADO");
+        }
+        System.out.println("El promedio es "+promedio);
+
+
+
+//        String userName = myObj.nextLine();  // Read user input
+
         //Ejercicio 1 - Hola Mundo
 
         // Ejercicio 2 - Ingrese dos numeros y muestre el resultado de la suma
@@ -8,13 +34,13 @@ public class primero {
         //  Ejercico 3 - Ingrese tres numeros y  muestre el mayor
     }
     /*
-     TIPO DE VARIABLES:
+     TIPO DE VARIABLES - ocupa un espacio de memoria:
      * Primitivas:
      - Numerico
         - enteros
             byte -128 a 127
             short -32mil a 32mil
-            int -2.1 mil milloes a 2.2 mil millones
+            int -2.1 mil millones a 2.2 mil millones
             long -2 con 63 ceros a 2 con 63 ceros (subFijo L o l )
         - decimales
             float 6 a 7 cifras decimales (subFijo F o f )
