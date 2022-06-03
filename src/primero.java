9cf4d8b7bbc4179c997b992959139c74fab85737
Pepe Moyano <josemariamoyano.ar@gmail.com>
2022-06-03 09:21:46 +0200

    agregar informacion sobre tipo de datos

--- src/primero.java
+++ src/primero.java
@@ -4,24 +4,26 @@ public class primero {
 
     }
     /*
-     TIPO DE DATOS:
-     * Nativos:
+     TIPO DE VARIABLES:
+     * Primitivas:
      - Numerico
-        byte
-        short
-        int
-        float
-        double
-        long
+        - enteros
+            byte -128 a 127
+            short -32mil a 32mil
+            int -2.1 mil milloes a 2.2 mil millones
+            long -2 con 63 ceros a 2 con 63 ceros (subFijo L o l )
+        - decimales
+            float 6 a 7 cifras decimales (subFijo F o f )
+            double 15 cifras decimales
      - Caracter
-        char
+        char - caracter entre comillas simples, numero representado en codigo ascii 0 to 65535
      - Booleano (logico)
-        boolean
-     * Objetos - instancia de una clase
+        boolean - Solo dos valores true o false
+     * Referencia a Objetos - instancia de una clase
        String -> Cadena de caracteres
 
-     *Loops / Condicionales
-        if ()
+     * Operadores Logicos / Loops / Condicionales
+        if (CONDICION) // la condicion siempre devuelve un boolean
           ...
           else
           ....
