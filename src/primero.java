a32879ba365da6ebd07f18bc2d17b2dab83e9051
Pato <patriciogranelli@gmail.com>
2022-06-04 10:16:34 +0200

    Ejercicio 1, 2 y 3 Más el mayor de 10 números

--- src/primero.java
+++ src/primero.java
@@ -3,7 +3,25 @@ import java.util.Scanner;
 public class primero {
 
     public static void main(String[] args) {
+        //Estoy comentandotodo porque no se como empezar una hoja nueva
+        Scanner EntradaScanner = new Scanner(System.in);
+        int maximo;
+        int num;
+        int mayor = 0;
+        int cont;
 
+        System.out.println("Inserte el máximo de números");
+        maximo = EntradaScanner.nextInt();
+
+        for (cont = 0; cont < maximo; cont++) {
+            System.out.println("Inserte un número");
+            num = EntradaScanner.nextInt();
+            if (num > mayor) {
+                mayor = num;
+            }
+        }
+        System.out.println("El número mayor es " +mayor);
+    }
         /*Comento esto para seguir con el tercero
         //Ejercicio 1 - Hola Mundo
             System.out.println("Hola Mundo");
@@ -23,54 +41,4 @@ public class primero {
         System.out.println ("Ingrese el tercer número");
         Teclado = EntradaScanner.nextLine ();*/
 
-        int Numero1;
-        int Numero2;
-        int Numero3;
-
-        Scanner EntradaScanner = new Scanner (System.in);
-        System.out.println ("Ingrese el primer número");
-        Numero1 = EntradaScanner.nextInt ();
-        System.out.println ("Ingrese el segundo número");
-        Numero2 = EntradaScanner.nextInt ();
-        System.out.println ("Ingrese el tercer número");
-        Numero3 = EntradaScanner.nextInt ();
-
-        int NumeroMayor = Numero1;
-
-        if (Numero2 > NumeroMayor) {
-            NumeroMayor = Numero2;
-        }
-        if (Numero3 > NumeroMayor) {
-            NumeroMayor = Numero3;
-        }
-        System.out.println("El número mayor es "+NumeroMayor);
-
-    //No se si me fui muy largo con la info o si el "int" está bien usado, robé info de internet
-        // y de un curso que estuve mirando en Youtube, así que si algo de la fórmula está mal, le echamos la culpa a él
-    }
-    /*
-     TIPO DE VARIABLES:
-     * Primitivas:
-     - Numerico
-        - enteros
-            byte -128 a 127
-            short -32mil a 32mil
-            int -2.1 mil milloes a 2.2 mil millones
-            long -2 con 63 ceros a 2 con 63 ceros (subFijo L o l )
-        - decimales
-            float 6 a 7 cifras decimales (subFijo F o f )
-            double 15 cifras decimales
-     - Caracter
-        char - caracter entre comillas simples, numero representado en codigo ascii 0 to 65535
-     - Booleano (logico)
-        boolean - Solo dos valores true o false
-     * Referencia a Objetos - instancia de una clase
-       String -> Cadena de caracteres
-
-     * Operadores Logicos / Loops / Condicionales
-        if (CONDICION) // la condicion siempre devuelve un boolean
-          ...
-          else
-          ....
-     */
 }
