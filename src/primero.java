20ed7d043ca3ad31847b7273362a47e7a8f7b046
Guillermo <guillerubiolo@gmail.com>
2022-06-04 13:27:59 +0200

    Ejercicios clase 1

--- src/primero.java
+++ src/primero.java
@@ -1,14 +1,46 @@
+import java.util.Scanner;
+
 public class primero {
 
     public static void main(String[] args) {
         //Ejercicio 1 - Hola Mundo
+        /*
+        System.out.print("Hola mundo!");*/
 
         // Ejercicio 2 - Ingrese dos numeros y muestre el resultado de la suma
 
+        /*
+        byte n1;
+        n1=5;
+        byte n2;
+        n2=2;
+        byte resultado;
+        resultado = (byte) (n1 + n2);
+        System.out.print("La suma es " + resultado);
+    } */
         //  Ejercico 3 - Ingrese tres numeros y  muestre el mayor
+
+        int n1;
+        int n2;
+        int n3;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero: ");
+        n1 = scanner.nextInt();
+        System.out.println("Ingrese el segundo numero: ");
+        n2 = scanner.nextInt();
+        System.out.println("Ingrese el tercer numero: ");
+        n3 = scanner.nextInt();
+        int numeroMayor = n1;
+        if (n2 > numeroMayor)
+            System.out.print("El numero mayor es "+n2);
+        if (n3 > numeroMayor)
+            System.out.print("El numero mayor es "+n3);
     }
-    /*
-     TIPO DE VARIABLES:
+
+
+
+     /* TIPO DE VARIABLES:
      * Primitivas:
      - Numerico
         - enteros
