faa352975aedc1055f8641cbf416fc5e6c81fcc8
Pato <patriciogranelli@gmail.com>
2022-06-07 15:19:37 +0200

    Ejercicio 1, 2 y 3 Más el mayor de 10 números Ahora también te dice la primera posición donde se escribió el mayor

--- src/primero.java
+++ src/primero.java
@@ -1,8 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class primero {
-
     public static void main(String[] args) {
+        //Nuevo ejercicio, voy a tratar de hacer una lista con arrays a ver si puedo conseguir que
+        //digan en que puesto está el número mayor
+
+
+        Scanner EntradaScanner = new Scanner(System.in);
+
+        int maximo;
+        int num;
+        int mayor;
+        int cont;
+        int indice = 1;
+        int indicemayor = 1;
+
+        System.out.println("Inserte el máximo de números");
+        maximo = EntradaScanner.nextInt();
+
+        System.out.println("Inserte un número"); //Así no me podés pedir "ahora para 20" jeje
+        num = EntradaScanner.nextInt();
+        mayor = num;
+
+        for (cont = 0; cont <maximo-1; cont ++){ //Le puse -1 porque el primer número ya está es el "mayor"
+            System.out.println("Inserte un número"); //Así no me podés pedir "ahora para 20" jeje
+            num = EntradaScanner.nextInt();
+            indice = indice +1;
+            if (num > mayor) {
+                mayor = num; //Esta forma que ví en internet me parece aún más simple de la forma que le robé a tu hermano en el ejercicio anterior
+                indicemayor = indice;
+            }
+        }
+        System.out.println("El número mayor es " + mayor + " y fue escrito en la " + indicemayor + " posición");
+/*
+    //Ejercicio, encontrar el número mayor, sólo me falta sacar el int cont q está bastante al pedo
         //Estoy comentandotodo porque no se como empezar una hoja nueva
         Scanner EntradaScanner = new Scanner(System.in);
         int maximo;
@@ -25,7 +57,8 @@ public class primero {
             }
         }
         System.out.println("El número mayor es " +mayor);
-    }
+    /*
+     */
         /*Comento esto para seguir con el tercero
         //Ejercicio 1 - Hola Mundo
             System.out.println("Hola Mundo");
@@ -35,7 +68,7 @@ public class primero {
             System.out.println(Numero1 + Numero2);
         Y acá dejo de comentar esto */
 
-        //  Ejercicio 3 - Ingrese tres numeros y  muestre el mayor
+            //  Ejercicio 3 - Ingrese tres numeros y  muestre el mayor
         /*String Teclado;
         Scanner EntradaScanner = new Scanner (System.in);
         System.out.println ("Ingrese el primer número");
@@ -45,4 +78,5 @@ public class primero {
         System.out.println ("Ingrese el tercer número");
         Teclado = EntradaScanner.nextLine ();*/
 
-}
+        }
+    }
