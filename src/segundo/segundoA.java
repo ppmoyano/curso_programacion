1efcef320969a0a176899f9f4627cd7e0e9154d4
Pato <patriciogranelli@gmail.com>
2022-06-10 12:31:24 +0200

    Ejercicio SegundoA completo, creo. Pero eche moco y quise crear un método y ahora me quedó ahí abajo, no se como llamarlo y si lo borro rop todo :)

--- src/segundo/segundoA.java
+++ src/segundo/segundoA.java
@@ -11,9 +11,21 @@ c) Una vez que el programa finalice pregunte si quiere volver a realizarlo, y qu
  */
 
     public static void main(String[] args) {
+
+
+
+
+
+//dos cierres finales, ni hay que tocarlos
+    }
+}
+
+class mostrarNumeros {
+    public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
         int numero;
-
+        int opcion;
+        do {
         System.out.println("Ingrese un numero mayor a 0");
         numero = teclado.nextInt();
 
@@ -24,17 +36,13 @@ c) Una vez que el programa finalice pregunte si quiere volver a realizarlo, y qu
             }
             while (numero <= 0);
         }
-            for (int i = 0; i <= numero; i++) {
-                System.out.println(i);
-            }
-
-
-
-
-
-
-
-
-//dos cierres finales, ni hay que tocarlos
-    }
+        for (int i = 0; i <= numero; i++) {
+            System.out.println(i);
+        }
+    System.out.println("Ingrese cualquier número para volver a realizarlo");
+    System.out.println("caso contrario, ingrese 0");
+    opcion = teclado.nextInt();
+}
+while (opcion != 0);
+}
 }
