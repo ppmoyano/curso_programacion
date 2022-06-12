538498706f83c7f942a9e23e2e85528a3df7a598
Guillermo <guillerubiolo@gmail.com>
2022-06-12 23:27:34 +0200

    segundo b

--- src/segundo/segundoB.java
+++ src/segundo/segundoB.java
@@ -7,13 +7,40 @@ import java.util.Scanner;
 - Nos muestre todos los nombres que empiezan con la letra A
  */
 public class segundoB {
-  /*  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
         String cargarNombres;
-
+        String nombreMasLargo;
         String[] nombres = new String[6];
+        String nombreMasCorto;
         cargarNombresEnArray(nombres);
+        nombreMasLargo = calcularEnElArrayElMasLargo(nombres);
+        nombreMasCorto = calcularEnArrayElMasCorto(nombres);
+        System.out.println("El nombre mas largo es: " + nombreMasLargo);
+        System.out.println("El nombre mas corto es: " + nombreMasCorto);
+    }
+
+    private static String calcularEnArrayElMasCorto(String[] nombres) {
+          String nombreMasCorto="";
+          int menor = nombres[0].length();
+          for (int i=0; i < nombres.length; i++) {
+              if (nombres[i].length() < menor) {
+                   nombreMasCorto = nombres[i];
+              }
+          }
+        return nombreMasCorto;
+    }
+
+    private static String calcularEnElArrayElMasLargo(String[] nombres) {
+        String nombreMasLargo="";
+        int mayor = nombres[0].length();
+        for (int i = 0; i < nombres.length; i++) {
+
+            if (nombres[i].length() > mayor) {
+                nombreMasLargo = nombres[i];
+            }
+        }        return nombreMasLargo;
     }
 
     private static void cargarNombresEnArray(String[] nombres) {
@@ -23,18 +50,6 @@ public class segundoB {
             nombres[i] = teclado.next();
         }
 
+
     }
-
-    private static String calcularNombreMasLargo(String[] nombres) {
-        String nombreMasLargo="";
-        int mayor = nombres.length[0];
-        for (int i = 0; i < nombres.length; i++) {
-
-            if (nombres.length[i] > mayor) {
-                nombreMasLargo = nombres[i];
-
-            }
-        }        return nombreMasLargo;
-    }
-}*/
-//no esta terminado
+}
