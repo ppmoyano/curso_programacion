1be95c2ec14f75711e19f4df744a6e39cc7825ce
Guillermo <guillerubiolo@gmail.com>
2022-06-12 16:34:14 +0200

    segundo a , b y c

--- src/segundo/segundoA.java
+++ src/segundo/segundoA.java
@@ -5,46 +5,48 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class segundoA {
-/*
-a)Realice un programa donde ingrese un numero y se muestre todo los numeros desde 0 hasta el mismo numero inclusive.
-*/
-public static void main(String[] args) {
-    Scanner teclado = new Scanner(System.in);
+    /*
+    a)Realice un programa donde ingrese un numero y se muestre todo los numeros desde 0 hasta el mismo numero inclusive.
 
-    int numero;
+   // public static void main(String[] args) {
+     //   Scanner teclado = new Scanner(System.in);
 
-    System.out.println("Ingrese un numero: ");
-    numero = teclado.nextInt();
+        int numero;
 
-    for (int i =0; i <= numero; i++) {
-        System.out.println(i);
-    }
+        System.out.println("Ingrese un numero: ");
+        numero = teclado.nextInt();
 
-
+        for (int i = 0; i <= numero; i++) {
+            System.out.println(i);
+        }
 
 
 //b)Si el numero ingresado es menor a 0 mostrar un mensaje de error
 
-    if ( numero <= 0) {  System.out.println("ERROR");}  // No se porque si pongo 0, tambien me muestra 0 y ERROR
+        if (numero <= 0) {
+            System.out.println("ERROR");
+        }  // No se porque si pongo 0, tambien me muestra 0 y ERROR
 
     }
-
-
-
-
-
-
+*/
 
     //c) Una vez que el programa finalice pregunte si quiere volver a realizarlo, y que lo haga hasta que el usuario ingrese 0.
 
+    public static void main(String[] args) {
+    int opcion;
+    int numero;
+    Scanner teclado = new Scanner(System.in);
 
+    do{ System.out.println("Si desdea continuar presione cualquier numero. De lo contrario presione 0");
+        opcion= teclado.nextInt();
 
+        System.out.println("Ingrese un numero: ");
+        numero = teclado.nextInt();
 
-}
+        for (int i = 0; i <= numero; i++) {
+            System.out.println(i);
+        }
+    }while(opcion != 0);
+// Lo maximo que pude lograr despues de muchos intentos jaja
 
-
-
-
-
-
-
+}}
