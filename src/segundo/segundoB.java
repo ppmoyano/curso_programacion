cadc3c1cdd4dd2ec019deef6eee78c8cbe11f6a9
Guillermo <guillerubiolo@gmail.com>
2022-06-14 11:49:27 +0200

    segundo c

--- src/segundo/segundoB.java
+++ src/segundo/segundoB.java
@@ -1,11 +1,11 @@
-package segundo;
+/*package segundo;
 
 import java.util.Scanner;
-/*
+
 - Realice un programa donde ingrese todos los nombres de un curso y al final nos muestre el nombre mas largo
 - Nos muestre el nombre mas corto
 - Nos muestre todos los nombres que empiezan con la letra A
- */
+
 public class segundoB {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
@@ -73,3 +73,4 @@ public class segundoB {
 
     }
 }
+*/
