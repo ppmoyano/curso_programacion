79edbc8a97cd3e413e992ecfda6831b1a873bf97
pjmoyano <josemariamoyano.ar@gmail.com>
2022-06-14 17:59:25 +0200

    Agregando tercer ejercicio

--- src/tercero/ejercicio3a/Ejercicio3a.java
+++ src/tercero/ejercicio3a/Ejercicio3a.java
@@ -4,6 +4,7 @@ Este es un programa para registar los alumnos para la escuela primaria Carlos Pa
 En la escuela vamos registatondo todos los niños que se vienen a anotar.
 Se anota el nombre, apellido, y la edad. El niño no puede tener menos de 6 años ni mas de 8 para inscribirse.
 Si no cumple con esta condicion se le dice Que no cumple con la edad necaria.
+Una vez inscripto de muestra el mensaje - NIÑO REGISTRADO mas la informacion del niño.
 Y tenemos un cupo maximo de 5 alumnos, asi que si se llena ese cupo el programa termina con el mensaje "CUPO COMPLETO"
 Al finalizar el programa mostrar el mensaje "FIN DEL PROGRAMA"
  */
@@ -26,7 +27,7 @@ Ingrese el apellido:
 Rizzo
 Ingrese la edad:
 7
-NIÑO REGISTRADO
+NIÑO REGISTRADO - Sabri Rizzo de 7 años de edad
 Desea ingresar un niño: 1 - Si 2 - Salir
 1
 Ingrese el nombre:
@@ -44,7 +45,7 @@ Ingrese el apellido:
 Marmol
 Ingrese la edad:
 7
-NIÑO REGISTRADO
+NIÑO REGISTRADO - Pablo Marmol de 7 años de edad
 Desea ingresar un niño: 1 - Si 2 - Salir
 1
 Ingrese el nombre:
@@ -53,7 +54,7 @@ Ingrese el apellido:
 Picapiedra
 Ingrese la edad:
 7
-NIÑO REGISTRADO
+NIÑO REGISTRADO - Pedro Picapiedra de 7 años de edad
 Desea ingresar un niño: 1 - Si 2 - Salir
 1
 Ingrese el nombre:
@@ -62,16 +63,16 @@ Ingrese el apellido:
 Palma
 Ingrese la edad:
 7
-NIÑO REGISTRADO
+NIÑO REGISTRADO - Vilma Palma de 7 años de edad
 Desea ingresar un niño: 1 - Si 2 - Salir
 1
 Ingrese el nombre:
 Leon
 Ingrese el apellido:
-Corazon de
+Corazon
 Ingrese la edad:
 7
-NIÑO REGISTRADO
+NIÑO REGISTRADO - Leon Corazon de 7 años de edad
 CUPO COMPLETO
 Fin del programa
 
--- src/tercero/ejercicio3a/Persona.java
+++ src/tercero/ejercicio3a/Persona.java
@@ -1,4 +1,6 @@
 package tercero.ejercicio3a;
-
+/*
+Aqui tenemos que agregar los campos necesarios y un metodo para mostrar la informacion de la persona
+*/
 public class Persona {
 }
