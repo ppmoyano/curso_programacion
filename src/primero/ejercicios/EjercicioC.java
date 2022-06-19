19f9645a025316cbb44e63eb2b21a9cc6ff6b1c5
Sabri <sabri.rizzo@gmail.com>
2022-06-19 18:56:38 +0200

    ejercicio C

--- src/primero/ejercicios/EjercicioC.java
+++ src/primero/ejercicios/EjercicioC.java
@@ -8,11 +8,72 @@ Si hay mas impares que pares, muestre un mensaje GANARON LOS IMPARES
 Y si empataron, muestre un mensaje que diga EMPATE
 */
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class EjercicioC {
     public static void main(String[] args) {
-
-
+        int[] numeros = new int[largoDelArray()];
+        cargarNumerosEnElArray(numeros);
+        System.out.println("La suma de los numeros pares es: " + sumaDeNumerosPares(numeros));
+        System.out.println("La suma de los numeros impares es: " + sumaDeNumerosImpares(numeros));
+        gananParesOImpares(numeros);
     }
+    private static String gananParesOImpares(int[] numeros) {
+        if (sumaDeNumerosPares(numeros) > sumaDeNumerosImpares(numeros)) {
+            System.out.println("GANARON LOS PARES");
+        }
+        if (sumaDeNumerosPares(numeros) < sumaDeNumerosImpares(numeros)) {
+            System.out.println("GANARON LOS IMPARES");
+        }
+        if (sumaDeNumerosPares(numeros) == sumaDeNumerosImpares(numeros)) {
+            System.out.println("EMPATE");
+        }
+        return "";
+    }
+    private static int sumaDeNumerosImpares(int[] numeros) {
+        int sumaImpares = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if ((numeros[i] % 2) != 0) {
+                sumaImpares += numeros[i];
+            }
+        }
+        return sumaImpares;
+    }
+    private static int sumaDeNumerosPares(int[] numeros) {
+        int sumaPares = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if ((numeros[i] % 2) == 0) {
+                sumaPares += numeros[i];
+            }
+        }
+        return sumaPares;
+    }
+    private static void cargarNumerosEnElArray(int[] numeros) {
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese un numero: ");
+            numeros[i] = teclado.nextInt();
+            if ((numeros[i] % 2) == 0) {
+                System.out.println("NUMERO PAR");
+            } else {
+                System.out.println("NUMERO IMPAR");
+            }
+        }
+    }
+    private static int largoDelArray() {
+        Scanner teclado = new Scanner(System.in);
+        int cantidadDeNumeros = 0;
+        do {
+            System.out.println("Ingrese la cantidad de numeros q desea ingresar : ");
+            cantidadDeNumeros = teclado.nextInt();
+            if (cantidadDeNumeros < 1) {
+                System.out.println("no puede ingresar un numero menor a 1");
+            }
+        } while (cantidadDeNumeros < 1);
+        return cantidadDeNumeros;
+    }
+
 }
 /*
 Ejemplo:
