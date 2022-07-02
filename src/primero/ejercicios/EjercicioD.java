c9467d220ae8bc012d0295e6b68e101506c5d7bc
Sabri <sabri.rizzo@gmail.com>
2022-07-02 10:30:03 +0200

    ejercicio D

--- src/primero/ejercicios/EjercicioD.java
+++ src/primero/ejercicios/EjercicioD.java
@@ -7,9 +7,57 @@ Ejemplis de numeros primos hasta 20:
  */
 
 
+import java.util.Scanner;
+
 public class EjercicioD {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero = ingresarNumero();
+        esPrimo(numero);
+        String resultado;
+        resultado = losAnterioressonPrimos(numero);
+        System.out.println(resultado);
+        System.out.println("FIN DEL PROGRAMA");
+    }
+
+    private static String losAnterioressonPrimos(int numero) {
+        int todosLosNumeros = 1;
+        String resultado ="";
+        while (todosLosNumeros < numero) {
+            if (esPrimo(todosLosNumeros)) {
+                resultado += "  " + todosLosNumeros + " ";
+            }
+            todosLosNumeros++;
+        }
+
+        return resultado;
+    }
 
 
+    private static boolean esPrimo(int numero) {
+        boolean esNumeroPrimo = true;
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                esNumeroPrimo = false;
+            }
+        }
+        return esNumeroPrimo;
+    }
+
+
+    private static int ingresarNumero() {
+        Scanner teclado = new Scanner(System.in);
+        int numero = 0;
+        do {
+            System.out.println("Ingrese un numero: ");
+
+            numero = teclado.nextInt();
+            if (numero < 2) {
+                System.out.println("No puede ingresar un numero menor a 2 ");
+            }
+        } while (numero < 2);
+        return numero;
+    }
 }
 /*
 EJEMPLOS DE EJECUCION 1:
