caefb8c4dae5bbe2a92e3993fb38dadd02d90e6d
Guillermo <guillerubiolo@gmail.com>
2022-07-06 15:30:22 +0200

    Ejercicio3b

---
+++ src/tercero/ejercicio3b/Vip.java
@@ -0,0 +1,10 @@
+package tercero.ejercicio3b;
+
+import java.util.Scanner;
+
+public class Vip extends Cliente{
+    public void cargarCliente() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Bienvenido al Banco, que operacion desea realizar: ");
+    }
+}
